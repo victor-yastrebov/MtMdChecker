a779ed6d3263476710787f38b7b56f23879e7f49
va.yastrebov <va.yastrebov@tensor.ru>
2019-07-24 19:18:50 +0300

    Get rid of semicolons and useless whitespaces

--- check_mt_md.py
+++ check_mt_md.py
@@ -1,7 +1,7 @@
 """
    This script can be used to define using dumpbin.exe
    whether the library is compiled using /MT or /MD flag 
-   
+
    Author: Victor Yastrebov
    Initial work: https://github.com/victor-yastrebov/MtMdChecker
 """
@@ -26,7 +26,7 @@ def SystemExecute( cmd_line, verbose_output = False ) :
 
 # get rid of leading and trailing ' ' and \n characters 
 def preprocessString(fileName) :
-   return fileName.strip();
+   return fileName.strip()
 
 # create cmd line command for DumpBin.exe
 def createDumpBinCmd(fileName, pathToDumpBin) :
@@ -34,14 +34,14 @@ def createDumpBinCmd(fileName, pathToDumpBin) :
 
    outputFilename = "dirs_" + ntpath.basename(fileName) + ".txt"
    redirectOutputCmd = "> " + outputFilename
-   
+
    stringList = [pathToDumpBin, dumpBinFlag, fileName, redirectOutputCmd]
    # concatenate strings via ' '
    return [' '.join(string for string in stringList), outputFilename]
 
 # check whether library is build using MD or MT
 def checkMdOrMt(fileName) :
-   
+
    isMT = False
    isMD = False
 
@@ -60,30 +60,29 @@ def checkMdOrMt(fileName) :
       return "MD"
    else :
       return "UNKNOWN"
-   
+
 # entry point for application
 if __name__ == "__main__" :
-   
+
    # specify all pathes here
-   inputFileName = "input_example.txt";
-   outputFileName = "results.txt";
+   inputFileName = "input_example.txt"
+   outputFileName = "results.txt"
    pathToDumpBin = "C:\\\"Program Files (x86)\"\\\"Microsoft Visual Studio 14.0\"\\VC\\bin\\dumpbin.exe"
-   
-   with open(inputFileName) as fp:  
+
+   with open(inputFileName) as fp:
       for srcFileName in fp:
-         
+
          # do not process comments and empty lines
          if(srcFileName[0] == '#' or srcFileName[0] == '\n') :
             continue
-         
+
          preprocFileName = preprocessString(srcFileName)
 
-         [cmdLine, outputFile] = createDumpBinCmd(preprocFileName, pathToDumpBin);
+         [cmdLine, outputFile] = createDumpBinCmd(preprocFileName, pathToDumpBin)
          SystemExecute(cmdLine, False)
          statusMdMt = checkMdOrMt(outputFile)
-         
-         with open(outputFileName, "a") as outfile:
+
+         with open(outputFileName, "a") as outfile :
             outfile.write(preprocFileName + ": " + statusMdMt + "\n")
-         
+
    print("Done!")
-         
